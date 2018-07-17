d3cf6e99f2914576ce6581d7088523c5a6669d74
Andre Pires <andrecabralpires@gmail.com>
2018-07-17 10:53:39 +0100

    active record advanced

---
+++ app/models/doctor.rb
@@ -0,0 +1,11 @@
+class Doctor < ActiveRecord::Base
+  has_many :interns
+  has_many :consultations
+
+  has_many :patients, through: :consultations
+  validates :last_name, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :first_name, length: { minimum: 4}
+  validates :email, format:{ }
+
+end
